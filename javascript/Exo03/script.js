03cb3e4de862ed4489be99f22dea0cac8b462a10
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-06-13 16:00:31 +0200

    Added bonus to counter exo js

--- javascript/Exo03/script.js
+++ javascript/Exo03/script.js
@@ -1,11 +1,12 @@
+const increaseHTML = document.getElementById("increase");
 const upHTML = document.getElementById("up");
 const downHTML = document.getElementById("down");
 const counterHTML = document.querySelector("h2");
 
 upHTML.addEventListener("click", (e) => {
-    counterHTML.innerText = parseInt(counterHTML.innerText) + 1;
+    counterHTML.innerText = parseInt(counterHTML.innerText) + parseInt(increaseHTML.value);
 });
 
 downHTML.addEventListener("click", (e) => {
-    counterHTML.innerText = parseInt(counterHTML.innerText) - 1;
+    counterHTML.innerText = parseInt(counterHTML.innerText) - parseInt(increaseHTML.value);
 })
