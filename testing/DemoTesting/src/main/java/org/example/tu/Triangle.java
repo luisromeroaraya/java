2485417c0f8a98fb008e3c45ca9ced19b4d1a6db
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-07-22 10:06:05 +0200

    Finished Triangle Test Demo

--- testing/DemoTesting/src/main/java/org/example/tu/Triangle.java
+++ testing/DemoTesting/src/main/java/org/example/tu/Triangle.java
@@ -8,7 +8,10 @@ public class Triangle {
     // constructor
     public Triangle(int a, int b, int c) {
         if (a <= 0 || b <= 0 || c <= 0) {
-                throw new IllegalArgumentException("All values must be greater than 0.");
+            throw new IllegalArgumentException("All values must be greater than 0.");
+        }
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("The sum of the lengths of any two sides of a triangle must be greater than the length of the third side.");
         }
         this.a = a;
         this.b = b;
--- testing/DemoTesting/src/test/java/org/example/tu/TriangleTest.java
+++ testing/DemoTesting/src/test/java/org/example/tu/TriangleTest.java
@@ -7,32 +7,96 @@ import static org.junit.jupiter.api.Assertions.*; // we import static to use ass
 
 public class TriangleTest {
 
+    // Valid arguments tests
     @Test
-    @DisplayName("Valid arguments")
-    void validArguments() {
-        int a=1, b=2, c=1;
+    @DisplayName("Valid arguments greater than 0")
+    void validArgumentsGreaterThan0() {
+        int a=3, b=4, c=5; // (a <= 0 || b <= 0 || c <= 0) = FALSE => DOES NOT THROW EXCEPTION -> (3 + 4 <= 5 || 3 + 5 <= 4 || 4 + 5 <= 3) = FALSE => DOES NOT THROW EXCEPTION
         assertDoesNotThrow(()->{Triangle triangle = new Triangle(a,b,c);},"All values must be greater than 0.");
+    }
+
+    @Test
+    @DisplayName("Valid triangle sides")
+    void validArgumentsTriangleSides() {
+        int a=3, b=4, c=5; // (a <= 0 || b <= 0 || c <= 0) = FALSE => DOES NOT THROW EXCEPTION -> (3 + 4 <= 5 || 3 + 5 <= 4 || 4 + 5 <= 3) = FALSE => DOES NOT THROW EXCEPTION
+        assertDoesNotThrow(()->{Triangle triangle = new Triangle(a,b,c);},"The sum of the lengths of any two sides of a triangle must be greater than the length of the third side.");
     }
 
     @Test
     @DisplayName("Invalid arguments caused by value = 0")
     void invalidArgumentsCausedBy0() {
-        int a=0, b=2, c=1;
-        assertThrows(IllegalArgumentException.class, ()->{Triangle triangle = new Triangle(a,b,c);});
+        int a=0, b=4, c=5; // (a <= 0 || b <= 0 || c <= 0) = TRUE => THROWS EXCEPTION
+        assertEquals("All values must be greater than 0.", assertThrows(IllegalArgumentException.class, ()->{Triangle triangle = new Triangle(a,b,c);}).getMessage());
     }
 
     @Test
     @DisplayName("Invalid arguments caused by negative values")
     void invalidArgumentsCausedByNegative() {
-        int a=1, b=2, c=-1;
-        assertThrows(IllegalArgumentException.class, ()->{Triangle triangle = new Triangle(a,b,c);});
+        int a=3, b=-4, c=5; // (a <= 0 || b <= 0 || c <= 0) = TRUE => THROWS EXCEPTION
+        assertEquals("All values must be greater than 0.", assertThrows(IllegalArgumentException.class, ()->{Triangle triangle = new Triangle(a,b,c);}).getMessage());
     }
 
     @Test
+    @DisplayName("Invalid arguments caused by impossible triangle side values")
+    void invalidArgumentsCausedByImpossibleTriangleSideValues() {
+        int a=1, b=2, c=3; // (1 + 2 <= 3 || 3 + 5 <= 4 || 4 + 5 <= 3) = TRUE => THROWS EXCEPTION
+        assertEquals("The sum of the lengths of any two sides of a triangle must be greater than the length of the third side.", assertThrows(IllegalArgumentException.class, ()->{Triangle triangle = new Triangle(a,b,c);}).getMessage());
+    }
+
+    // type() method tests
+    @Test
     @DisplayName("type() for equilateral triangle success")
-    void equilateralTriangleSuccess() {
+    void typeEquilateralTriangleSuccess() {
         int a=3, b=3, c=3;
         assertEquals("equilateral", new Triangle(a,b,c).type());
     }
 
+    @Test
+    @DisplayName("type() for isosceles triangle success (a == b)")
+    void typeIsoscelesTriangleSuccess01() {
+        int a=3, b=3, c=5;
+        assertEquals("isosceles", new Triangle(a,b,c).type());
+    }
+
+    @Test
+    @DisplayName("type() for isosceles triangle success (a == c)")
+    void typeIsoscelesTriangleSuccess02() {
+        int a=3, b=5, c=3;
+        assertEquals("isosceles", new Triangle(a,b,c).type());
+    }
+
+    @Test
+    @DisplayName("type() for isosceles triangle success (b == c)")
+    void typeIsoscelesTriangleSuccess03() {
+        int a=5, b=3, c=3;
+        assertEquals("isosceles", new Triangle(a,b,c).type());
+    }
+
+    @Test
+    @DisplayName("type() for scalene triangle success")
+    void typeScaleneTriangleSuccess() {
+        int a=3, b=4, c=5;
+        assertEquals("scalene", new Triangle(a,b,c).type());
+    }
+
+    @Test
+    @DisplayName("type() for equilateral triangle fail")
+    void typeEquilateralTriangleFail() {
+        int a=3, b=4, c=5;
+        assertNotEquals("equilateral", new Triangle(a,b,c).type());
+    }
+
+    @Test
+    @DisplayName("type() for isosceles triangle fail")
+    void typeIsoscelesTriangleFail() {
+        int a=3, b=4, c=5;
+        assertNotEquals("isosceles", new Triangle(a,b,c).type());
+    }
+
+    @Test
+    @DisplayName("type() for scalene triangle fail")
+    void typeScaleneTriangleFail() {
+        int a=3, b=4, c=4;
+        assertNotEquals("scalene", new Triangle(a,b,c).type());
+    }
 }
