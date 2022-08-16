4617df2ebc5612fe7dccd9f366994ed5636f0949
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-08-16 14:14:02 +0200

    Test

--- src/main/java/com/example/demorest/tools/DataInit.java
+++ src/main/java/com/example/demorest/tools/DataInit.java
@@ -18,36 +18,36 @@ import java.util.Set;
 
 @Service
 public class DataInit implements InitializingBean {
-    private final ChildRepository childRepository;
-    private final TutorRepository tutorRepository;
-    private final UserRepository userRepository;
-
-    public DataInit(ChildRepository childRepository, TutorRepository tutorRepository, UserRepository userRepository) {
-        this.childRepository = childRepository;
-        this.tutorRepository = tutorRepository;
-        this.userRepository = userRepository;
-    }
-
-    private final List<Tutor> tutors = Arrays.asList(
-            new Tutor("Scarlett", "Johansson", "0123456789", new Address("Johansson Street", 123, "2E", "Brussels", "Brussels-Capital", 1000)),
-            new Tutor("Jennifer", "Anniston", "0123456789",  new Address("Anniston Street", 123, "2E", "Brussels", "Brussels-Capital", 1000)),
-            new Tutor("Julia", "Roberts", "0123456789",  new Address("Roberts Street", 123, "2E", "Ostend", "West Flanders", 1000))
-    );
-    private final List<Child> children = Arrays.asList(
-            new Child("Ben", "Affleck", LocalDate.of(2020,1,1), false, List.of("peanut", "aspirin"), Set.of(tutors.get(0))),
-            new Child("Tom", "Cruise", LocalDate.of(2020,1,1), false, List.of("egg"), Set.of(tutors.get(1))),
-            new Child("Leonardo", "Di Caprio", LocalDate.of(2020,1,1), false, List.of("peanut", "apple"), Set.of(tutors.get(2)))
-    );
-
-    private final List<User> users = Arrays.asList(
-            new User("user", new BCryptPasswordEncoder().encode("password"), true, List.of("USER")),
-            new User("admin", new BCryptPasswordEncoder().encode("password"), true, List.of("ADMIN"))
-    );
+//    private final ChildRepository childRepository;
+//    private final TutorRepository tutorRepository;
+//    private final UserRepository userRepository;
+//
+//    public DataInit(ChildRepository childRepository, TutorRepository tutorRepository, UserRepository userRepository) {
+//        this.childRepository = childRepository;
+//        this.tutorRepository = tutorRepository;
+//        this.userRepository = userRepository;
+//    }
+//
+//    private final List<Tutor> tutors = Arrays.asList(
+//            new Tutor("Scarlett", "Johansson", "0123456789", new Address("Johansson Street", 123, "2E", "Brussels", "Brussels-Capital", 1000)),
+//            new Tutor("Jennifer", "Anniston", "0123456789",  new Address("Anniston Street", 123, "2E", "Brussels", "Brussels-Capital", 1000)),
+//            new Tutor("Julia", "Roberts", "0123456789",  new Address("Roberts Street", 123, "2E", "Ostend", "West Flanders", 1000))
+//    );
+//    private final List<Child> children = Arrays.asList(
+//            new Child("Ben", "Affleck", LocalDate.of(2020,1,1), false, List.of("peanut", "aspirin"), Set.of(tutors.get(0))),
+//            new Child("Tom", "Cruise", LocalDate.of(2020,1,1), false, List.of("egg"), Set.of(tutors.get(1))),
+//            new Child("Leonardo", "Di Caprio", LocalDate.of(2020,1,1), false, List.of("peanut", "apple"), Set.of(tutors.get(2)))
+//    );
+//
+//    private final List<User> users = Arrays.asList(
+//            new User("user", new BCryptPasswordEncoder().encode("password"), true, List.of("USER")),
+//            new User("admin", new BCryptPasswordEncoder().encode("password"), true, List.of("ADMIN"))
+//    );
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        tutorRepository.saveAll(tutors);
-        childRepository.saveAll(children);
-        userRepository.saveAll(users);
+//        tutorRepository.saveAll(tutors);
+//        childRepository.saveAll(children);
+//        userRepository.saveAll(users);
     }
 }
