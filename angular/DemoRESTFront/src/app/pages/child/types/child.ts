d3d28a6621fe88e4180d3577951924394d721017
luisromeroaraya <luisromeroaraya@gmail.com>
2022-08-23 21:22:42 +0200

    Added views to Tutor in DemoRESTFront

---
+++ angular/DemoRESTFront/src/app/pages/child/types/child.ts
@@ -0,0 +1,9 @@
+export interface Child {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  toilet: boolean;
+  allergies: any[];
+  tutorsId: number[];
+}
--- angular/DemoRESTFront/src/app/pages/tutor/components/tutor-list/tutor-list.component.ts
+++ angular/DemoRESTFront/src/app/pages/tutor/components/tutor-list/tutor-list.component.ts
@@ -9,15 +9,26 @@ import {Tutor} from "../../types/tutor";
 })
 export class TutorListComponent implements OnInit {
   // variables
-  tutors?: any;
+  private _tutors: Tutor[] = [];
 
   // constructor
   constructor(private _http: HttpClient) { }
 
-  // methods
+  // getters
+  get Tutors(): Tutor[] {
+    return this._tutors;
+  }
+
+// methods
   ngOnInit(): void {
     const params = new HttpHeaders().append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImV4cCI6MTY2MjEwNzEzMn0.7wil4GDahiidJiw-fjju5fGZnxuY-GFYwHVoczVEL6uDgPCo-SycvxaZWirqvEacyZh_JU8u_DdN_BCNP0JOqA");
     // this._http.get('https://demo-rest-springboot.herokuapp.com/tutors/all', {headers: params}).subscribe(data => console.log(data));
-    this._http.get('http://localhost:8080/tutors/all', {headers: params}).subscribe(data => this.tutors=data);
+    this._http.get('http://localhost:8080/tutors/all', {headers: params}).subscribe(data => {
+      const response: any = data;
+      response.forEach((e: Tutor) => {
+        let tutor: Tutor = e;
+        this._tutors.push(tutor);
+      })
+    });
   }
 }
